751bebc631a13b5f67f0e6af224842e3ee034ca7
Lemeza <nick_stolk9@hotmail.com>
2019-02-15 10:07:30 +0100

    Add use strict to exchange.js and price.js

--- modules/exchange.js
+++ modules/exchange.js
@@ -1,3 +1,5 @@
+"use strict";
+
 module.exports = class Exchange {
 
   constructor(id, name, country) {
--- modules/price.js
+++ modules/price.js
@@ -1,3 +1,5 @@
+"use strict";
+
 module.exports = class Price {
 
   constructor(id, exchangeId, exchangePrice, cryptocurrencyId, date) {
