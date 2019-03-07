06992e958bed6749384f85c9a8365bf5e0278081
Lemeza <nick_stolk9@hotmail.com>
2019-03-07 11:33:04 +0100

    Add REST methods for cryptocurrency class

--- index.js
+++ index.js
@@ -8,16 +8,133 @@ let bodyParser = require('body-parser');
 
 app.use(bodyParser.json());
 
-app.use(function(req, res, next) {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Methods", "POST, GET, DELETE, PUT");
-  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-  next();
+app.use(function (req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Methods", "POST, GET, DELETE, PUT");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
 });
 
 const connection = mysql.createConnection({
-  host: 'localhost',
-  user: 'root',
-  password: 'root',
-  database: 'molveno'
+    host: 'localhost',
+    user: 'nick',
+    password: 'nick2018!',
+    database: 'personalcase'
+});
+
+connection.connect((err) => {
+    if (err) {
+        throw err;
+    }
+    else {
+        console.log(`Connected!`);
+    }
+});
+
+app.get('/api/cryptocurrency', (req, res) => {
+    connection.query('SELECT * FROM cryptocurrency', (err, cryptocurrency) => {
+        if (!err) {
+            res.setHeader('Content-Type', 'application/json');
+            res.end(JSON.stringify(cryptocurrency));
+        } else {
+            throw err;
+        }
+    });
+});
+
+app.get('/api/cryptocurrency/:id', (req, res) => {
+    let id = req.params.id;
+    connection.query('SELECT * FROM cryptocurrency WHERE id = ?', id, (err, rows) => {
+        if (!err) {
+            let cryptocurrency = rows[0];
+            if (cryptocurrency) {
+                console.log(`Cryptocurrency ${id} returned (${req.connection.remoteAddress})!`);
+                res.setHeader('Content-Type', 'application/json');
+                res.end(JSON.stringify(cryptocurrency));
+            } else {
+                console.log(`Cryptocurrency ${id} doesn't exist (${req.connection.remoteAddress})!`);
+                res.setHeader('Content-Type', 'application/json');
+                res.status(404).end();
+            }
+        } else {
+            throw err;
+        }
+    });
+});
+
+app.post('/api/cryptocurrency', (req, res) => {
+    let cryptocurrency = req.body;
+    connection.query('INSERT INTO cryptocurrency SET ?', cryptocurrency, (err, result) => {
+        if (!err) {
+            res.setHeader('Content-Type', 'application/json');
+            connection.query('SELECT * FROM cryptocurrency WHERE id = ?', result.insertId, (err, rows) => {
+                if (!err) {
+                    let cryptocurrency = rows[0];
+                    if (cryptocurrency) {
+                        res.setHeader('Content-Type', 'application/json');
+                        res.status(201).end(JSON.stringify(cryptocurrency));
+                        console.log(`Cryptocurrency ${JSON.stringify(cryptocurrency)} added (${req.connection.remoteAddress})!`);
+                    } else {
+                        res.setHeader('Content-Type', 'application/json');
+                        res.status(404).end();
+                    }
+                } else {
+                    throw err;
+                }
+            });
+        } else {
+            throw err;
+        }
+    });
+});
+
+app.put('/api/cryptocurrency/:id', (req, res) => {
+    let id = req.params.id
+    let inputCrypto = req.body;
+    console.log(`: Changing cryptocurrency with id: ${id}`);
+    connection.query(
+        'UPDATE cryptocurrency SET name = ?, supply = ?, protocol = ? WHERE id = ?',
+        [inputCrypto.name, inputCrypto.supply, inputCrypto.protocol, id],
+        (err, result) => {
+            if (!err) {
+                console.log(`: Changed ${result.changedRows} row(s)`);
+                connection.query('SELECT * FROM cryptocurrency WHERE id = ?', [id], (err, rows) => {
+                    if (!err) {
+                        console.log('Data received from Database:');
+                        let cryptocurrency = rows[0];
+                        console.log(cryptocurrency);
+                        if (cryptocurrency) {
+                            res.setHeader('Content-Type', 'application/json');
+                            res.end(JSON.stringify(cryptocurrency));
+                        } else {
+                            res.setHeader('Content-Type', 'application/json');
+                            console.log(`: Not found! (Request from ${req.connection.remoteAddress})`);
+                            res.status(404).end();
+                        }
+                    } else {
+                        throw err;
+                    }
+                });
+            }
+            else {
+                throw err;
+            }
+        }
+    );
+});
+
+app.delete('/api/cryptocurrency/:id', (req, res) => {
+    let id = req.params.id;
+    connection.query('DELETE FROM cryptocurrency WHERE id = ?', [id], (err, result) => {
+        if (!err) {
+            console.log(`Deleted ${result.affectedRows} row(s)`);
+            res.status(204).end();
+        } else {
+            throw err;
+        }
+    });
+});
+
+let server = app.listen(8081, () => {
+    console.log("Rest app listening at http://%s:%s", server.address().address, server.address().port);
 });
