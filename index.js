e8994e83de83ac12b87389cc4ea2caab9d84d30f
Lemeza <nick_stolk9@hotmail.com>
2019-03-08 11:24:35 +0100

    Add price class to REST service and update sql dump

--- index.js
+++ index.js
@@ -239,6 +239,110 @@ app.delete('/api/exchange/:id', (req, res) => {
     });
 });
 
+app.get('/api/price', (req, res) => {
+    connection.query('SELECT * FROM price', (err, price) => {
+        if (!err) {
+            res.setHeader('Content-Type', 'application/json');
+            res.end(JSON.stringify(price));
+        } else {
+            throw err;
+        }
+    });
+});
+
+app.get('/api/price/:id', (req, res) => {
+    let id = req.params.id;
+    connection.query('SELECT * FROM price WHERE id = ?', id, (err, rows) => {
+        if (!err) {
+            let price = rows[0];
+            if (price) {
+                console.log(`Price ${id} returned (${req.connection.remoteAddress})!`);
+                res.setHeader('Content-Type', 'application/json');
+                res.end(JSON.stringify(price));
+            } else {
+                console.log(`Price ${id} doesn't exist (${req.connection.remoteAddress})!`);
+                res.setHeader('Content-Type', 'application/json');
+                res.status(404).end();
+            }
+        } else {
+            throw err;
+        }
+    });
+});
+
+app.post('/api/price', (req, res) => {
+    let price = req.body;
+    connection.query('INSERT INTO price SET ?', price, (err, result) => {
+        if (!err) {
+            res.setHeader('Content-Type', 'application/json');
+            connection.query('SELECT * FROM price WHERE id = ?', result.insertId, (err, rows) => {
+                if (!err) {
+                    let price = rows[0];
+                    if (price) {
+                        res.setHeader('Content-Type', 'application/json');
+                        res.status(201).end(JSON.stringify(price));
+                        console.log(`Price ${JSON.stringify(price)} added (${req.connection.remoteAddress})!`);
+                    } else {
+                        res.setHeader('Content-Type', 'application/json');
+                        res.status(404).end();
+                    }
+                } else {
+                    throw err;
+                }
+            });
+        } else {
+            throw err;
+        }
+    });
+});
+
+app.put('/api/price/:id', (req, res) => {
+    let id = req.params.id
+    let inputPrice = req.body;
+    console.log(`Changing price with id: ${id}`);
+    connection.query(
+        'UPDATE price SET cryptocurrency_id = ?, exchange_id = ?, exchangePrice = ?, date = ? WHERE id = ?',
+        [inputPrice.cryptocurrency_id, inputPrice.exchange_id, inputPrice.exchangePrice, inputPrice.date, id],
+        (err, result) => {
+            if (!err) {
+                console.log(`Changed ${result.changedRows} row(s)`);
+                connection.query('SELECT * FROM price WHERE id = ?', [id], (err, rows) => {
+                    if (!err) {
+                        console.log('Data received from Database:');
+                        let price = rows[0];
+                        console.log(price);
+                        if (price) {
+                            res.setHeader('Content-Type', 'application/json');
+                            res.end(JSON.stringify(price));
+                        } else {
+                            res.setHeader('Content-Type', 'application/json');
+                            console.log(`: Not found! (Request from ${req.connection.remoteAddress})`);
+                            res.status(404).end();
+                        }
+                    } else {
+                        throw err;
+                    }
+                });
+            }
+            else {
+                throw err;
+            }
+        }
+    );
+});
+
+app.delete('/api/price/:id', (req, res) => {
+    let id = req.params.id;
+    connection.query('DELETE FROM price WHERE id = ?', [id], (err, result) => {
+        if (!err) {
+            console.log(`Deleted ${result.affectedRows} row(s)`);
+            res.status(204).end();
+        } else {
+            throw err;
+        }
+    });
+});
+
 let server = app.listen(8081, () => {
     console.log("Rest app listening at http://%s:%s", server.address().address, server.address().port);
 });
